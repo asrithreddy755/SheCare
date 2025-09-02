66cd301c1cb1e5a090bd10bc497ee30bd4e420d1
Satti asrith Reddy <sattiasrithreddy@gmail.com>
2025-09-02 18:15:28 +0000

    remove the forgot password option

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -57,12 +57,6 @@ export default function LoginPage() {
             <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="#"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    Forgot your password?
-                  </Link>
                 </div>
                 <div className="relative">
                   <Input 
